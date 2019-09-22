c0fe626021da74f525b6e45ff5fdade80384238b
Vincent Rubinetti <vince.rubinetti@gmail.com>
2019-09-21 20:45:33 -0400

    deploy app

---
+++ precache-manifest.f4397ccd51212c77adf5522c11de70c5.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6aea95e23b64d24c4391206698fd637",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd4e1bf5bb2cbdafc683",
+    "url": "./static/css/main.css"
+  },
+  {
+    "revision": "fd4e1bf5bb2cbdafc683",
+    "url": "./static/js/main.js"
+  }
+]);
